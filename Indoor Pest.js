e34b1433ef7327cf3498519d83769468281bc1ca
Paul Saya <87011258+PaulSaya@users.noreply.github.com>
2022-11-14 16:37:53 +0200

    Upload the new Indoor Pest.js

---
+++ Indoor Pest.js
@@ -0,0 +1,40 @@
+<!Doctype html>
+
+<script> 
+
+<style> 
+html{
+
+	font-size: 60px;
+	background-color:
+	font-family: Inverted Square;
+	
+}
+</style>
+<header>
+<body>
+<section>
+<h> Pest or Nature </h>
+<hr>
+<h3> what do you think nature is trying to say </h3>
+<br>
+<p> Potential there are are a bunch of pest in the world so you should 
+make treated unqaulity because can scare people </p> 
+<br>
+<p> There was a posionist frog in the bathroom please tell me if you have to call someone 
+becasue it was also in the bedroom where most people do not get a chance to wake up. </p>
+<img src = "https://i.natgeofe.com/n/fcab8efa-5832-404c-964f-fc3a798fdd44/poison-dart-frogs_thumb_square.jpg"/>
+<p> But some people do hold these creatures, its probably science. </p> 
+<ul><li>  
+<p> There are bull frogs two of which that have been seen. But there where outdoors. I took it too the bin  </p>
+<img src = " https://cdn.britannica.com/75/6975-004-C5803E33/Bullfrog.jpg?w=400&h=300&c=crop"/>
+
+</ul>
+<p> Here is a link about the frogs </p>
+<h3> Rats or Mice </h3>
+<p> Some animals should not be seen when not alive there was a mice that was like this. It was kept in a place that need to be witnessed, its completely return to its maker. </p>
+</section>
+</body>
+</header>
+<cite> Copyright </cite>
+</html>
